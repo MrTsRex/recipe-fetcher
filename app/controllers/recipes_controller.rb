1f71074827af2189ba5be26e42c996a16ef20da8
Sandesh Joshi <sandeshjoshi5@gmail.com>
2017-12-24 17:39:30 -0500

    controller-modified

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-  	@search_term = 'chocolate'
+  	@search_term = params[:looking_for] ||'chocolate'
   	@recipes = Recipes.for(@search_term)
   end
 end
